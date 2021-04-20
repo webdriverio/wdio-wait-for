5757c641f448cb1ffd2ee40d081cb00f7efe5901
Yevhen Laichenkov <elaichenkov@gmail.com>
2021-04-20 11:33:35 +0300

    update(tests): fix flaky sizeOfElementsToBe tests

--- tests/element/sizeOfElementsToBe.test.ts
+++ tests/element/sizeOfElementsToBe.test.ts
@@ -1,27 +1,24 @@
 import { sizeOfElementsToBe } from '../../src';
 
 const selector = 'button';
-const defaultWidth = 147;
-const defaultHeight = 46;
+let defaultWidth = 147;
+let defaultHeight = 46;
 
 describe('sizeOfElementsToBe', () => {
   beforeEach(async () => await browser.url('/add_remove_elements/'));
 
   it('should verify that method returns true when size of element to be', async () => {
+    defaultWidth = (await (await $('button')).getSize()).width;
+
     await browser.execute('document.querySelector("button").style.height = "300px"');
-    // await browser.waitUntil(sizeOfElementsToBe(selector, { width: defaultWidth, height: 300 }));
-    await browser.pause(5000);
-    console.log('<<<<<<< SIZE: ', await (await $('button')).getSize());
 
     expect(await sizeOfElementsToBe(selector, { width: defaultWidth, height: 300 })()).toBe(true);
   });
 
   it('should verify that method returns true when size of element to be', async () => {
-    await browser.execute('document.querySelector("button").style.width = "200px"');
-    // await browser.waitUntil(sizeOfElementsToBe(selector, { width: 200, height: defaultHeight }));
-    await browser.pause(5000);
-    console.log('<<<<<<< SIZE: ', await (await $('button')).getSize());
+    defaultHeight = (await (await $('button')).getSize()).height;
 
+    await browser.execute('document.querySelector("button").style.width = "200px"');
     expect(await sizeOfElementsToBe(selector, { width: 200, height: defaultHeight })()).toBe(true);
   });
 
