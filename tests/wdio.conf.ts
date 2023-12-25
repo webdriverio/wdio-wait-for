41569a724fb51075d43b803586487019ead0cb4a
Christian Bromann <git@bromann.dev>
2023-12-25 12:08:46 -0800

    remove node-chromedriver as dependency

--- tests/wdio.conf.ts
+++ tests/wdio.conf.ts
@@ -1,33 +1,32 @@
 export const config: WebdriverIO.Config = {
-  specs: ['./**/*.test.ts'],
-  exclude: ['./multiremote/multiremote.test.ts'],
-  maxInstances: 10,
-  capabilities: [
-    {
-      maxInstances: 10,
-      browserName: 'chrome',
-      acceptInsecureCerts: true,
-      'goog:chromeOptions': {
-        args: [
-          '--headless',
-          '--no-sandbox',
-          '--disable-dev-shm-usage',
-          '--enable-precise-memory-info',
-          '--window-size=1280,900',
-        ],
-      },
+    specs: ['./**/*.test.ts'],
+    exclude: ['./multiremote/multiremote.test.ts'],
+    maxInstances: 10,
+    capabilities: [
+        {
+            maxInstances: 10,
+            browserName: 'chrome',
+            acceptInsecureCerts: true,
+            'goog:chromeOptions': {
+                args: [
+                    '--headless',
+                    '--no-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--enable-precise-memory-info',
+                    '--window-size=1280,900',
+                ],
+            },
+        },
+    ],
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://the-internet.herokuapp.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'jasmine',
+    reporters: ['spec'],
+    jasmineOpts: {
+        defaultTimeoutInterval: 60000,
     },
-  ],
-  logLevel: 'info',
-  bail: 0,
-  baseUrl: 'https://the-internet.herokuapp.com',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 120000,
-  connectionRetryCount: 3,
-  services: ['chromedriver'],
-  framework: 'jasmine',
-  reporters: ['spec'],
-  jasmineOpts: {
-    defaultTimeoutInterval: 60000,
-  },
 }
--- tests/wdio.multiremote.conf.ts
+++ tests/wdio.multiremote.conf.ts
@@ -1,60 +1,45 @@
 export const config: WebdriverIO.Config = {
-  runner: 'local',
-
-  specs: ['./multiremote/multiremote.test.ts'],
-
-  maxInstances: 10,
-
-  capabilities: {
-    browserA: {
-      capabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          args: [
-            '--headless',
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--enable-precise-memory-info',
-            '--window-size=1280,900',
-          ],
+    specs: ['./multiremote/multiremote.test.ts'],
+    maxInstances: 10,
+    capabilities: {
+        browserA: {
+            capabilities: {
+                browserName: 'chrome',
+                'goog:chromeOptions': {
+                    args: [
+                        '--headless',
+                        '--no-sandbox',
+                        '--disable-dev-shm-usage',
+                        '--enable-precise-memory-info',
+                        '--window-size=1280,900',
+                    ],
+                },
+            },
         },
-      },
-    },
-    browserB: {
-      capabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
-          args: [
-            '--headless',
-            '--no-sandbox',
-            '--disable-dev-shm-usage',
-            '--enable-precise-memory-info',
-            '--window-size=1280,900',
-          ],
+        browserB: {
+            capabilities: {
+                browserName: 'chrome',
+                'goog:chromeOptions': {
+                    args: [
+                        '--headless',
+                        '--no-sandbox',
+                        '--disable-dev-shm-usage',
+                        '--enable-precise-memory-info',
+                        '--window-size=1280,900',
+                    ],
+                },
+            },
         },
-      },
     },
-  },
-
-  logLevel: 'info',
-
-  bail: 0,
-
-  baseUrl: 'https://the-internet.herokuapp.com',
-
-  waitforTimeout: 10000,
-
-  connectionRetryTimeout: 120000,
-
-  connectionRetryCount: 3,
-
-  services: ['chromedriver'],
-
-  framework: 'jasmine',
-
-  reporters: ['spec'],
-
-  jasmineOpts: {
-    defaultTimeoutInterval: 60000,
-  },
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'https://the-internet.herokuapp.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    framework: 'jasmine',
+    reporters: ['spec'],
+    jasmineOpts: {
+        defaultTimeoutInterval: 60000,
+    },
 }
