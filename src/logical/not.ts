9ab477667c618b6ca9ebd7692e7c91ea5ec4cb3c
Muhammad Hammad Faisal <hammadfaisal178@gmail.com>
2024-02-07 05:05:41 +0500

    logical not bug-fix (#800)

--- src/logical/not.ts
+++ src/logical/not.ts
@@ -6,12 +6,12 @@
  *
  * @param {!Function} expectedCondition The function to check
  *
- * @returns {!function} An expected condition that returns that returns the negated value.
+ * @returns {!function} An expected condition that returns the negated value.
  */
 
 export function not(expectedCondition: () => Promise<boolean>): () => Promise<boolean> {
-  return async (): Promise<boolean> => {
-    const result = await expectedCondition()
+  return async function (this:WebdriverIO.Browser): Promise<boolean> {
+    const result = await expectedCondition.call(this)
 
     return !result
   }
