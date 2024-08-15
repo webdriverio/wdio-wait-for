7bad0dc2d3284100ba614cf0cfcbbb65e83d38a9
Christian Bromann <git@bromann.dev>
2024-08-15 09:36:03 -0700

    feat: update to v9 (#937)
    * feat: update to v9
    
    * update deps

--- src/utils/getElements.ts
+++ src/utils/getElements.ts
@@ -1,6 +1,7 @@
-import type { ChainablePromiseArray } from 'webdriverio'
 import type { StringOrElements } from './element.types'
 
-export const getElements = (selectorOrElementArray: StringOrElements): ChainablePromiseArray<WebdriverIO.ElementArray> => {
-  return typeof selectorOrElementArray === 'string' ? $$(selectorOrElementArray) : selectorOrElementArray
+export const getElements = (selectorOrElementArray: StringOrElements): Promise<WebdriverIO.ElementArray> => {
+  return typeof selectorOrElementArray === 'string'
+    ? $$(selectorOrElementArray).getElements()
+    : selectorOrElementArray.getElements()
 }
